c251d6160b02798d2ed760d48b0d1970befbf83d
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-05-18 15:45:10 +0530

    submitting code-Thu May 18 2023 15:45:09 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,38 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import "./App.css";
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
+  return (
+    <div className="App">
+      <div id='review-heading'>
+      <h1>Reviews</h1>
+      <Carousel 
+      showArrows={true}
+      width="700px"
+      infiniteLoop
+      swipeable={true}
+       id='review-container' key={Math.random(100)*3}>
+        {
+            data.map((val,index)=>(
+               <div key={val.id} className="card">
+                <div className="textInfo">
+                  <p id={'person-'+index}>
+                    Name :  {val.name}
+                  </p>
+                  <p id={'title-'+index} style={{fontSize:'20px'}}>Title : {val.title}</p>
+                  <p>"<i>{val.quote}</i>"</p>
+                </div>
+                <div className="imgInfo">
+                  <img src={val.image} height={600}   alt='personImg'/>
+                </div>
+               </div>
+            ))
+        }
+      </Carousel>
+    </div>
+    </div>
+  );
+}
+
+export default App;
